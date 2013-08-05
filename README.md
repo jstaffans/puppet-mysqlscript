9f216789d343cbe5d90f350ac057838edf932a6d
jstaffans <johannes.staffans@gmail.com>
2013-08-05 20:29:13 +0300

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ puppet-mysqlscript
 ==================
 
 Puppet module for running arbitrary mysql scripts
+
+
+## Usage:
+
+```
+  class { 'mysqlscript':
+    snippets_dir => '/opt/my-sql-snippets',
+  }
+
+  mysqlscript::run_script { 'create-db.sql': }
+```
---
+++ manifests/init.pp
@@ -0,0 +1,28 @@
+class mysqlscript (
+  $snippets_dir = undef,
+  ) {
+
+  define run_script() {
+    $script = "$mysqlscript::snippets_dir/$name"
+
+    file { $script:
+      mode => "600",
+    }
+
+    exec {"mysql-import-$name":
+      path    => ['/bin', '/sbin', '/usr/bin'],
+      command => "mysql --defaults-file=/root/.my.cnf $db < $script && touch $script.semaphore", 
+      creates => "$script.semaphore",
+      require => File[$script],
+      timeout => '0',
+    }
+  }
+
+  file { $snippets_dir:
+      ensure   => directory,
+      mode => "700",
+  }
+
+  Class['mysql'] -> Class['mysqlscript']
+
+}
